562ac8e0be2115555b26435930e559df07df28a5
hariharan0033 <hariharan00332810@gmail.com>
2024-02-27 19:05:13 +0530

    Implemented Increment and Decrement functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 const incrementFunction = () => {
+    var  numberString = document.getElementById("counter-number");
+    var number = Number(numberString.textContent);
+    numberString.textContent = number+1;
     console.log(`increment logged!`)
 }
 
 const decrementFunction = () => {
+    var  numberString = document.getElementById("counter-number");
+    var number = Number(numberString.textContent);
+    numberString.textContent = number-1;
     console.log(`decrement logged!`)
 }
